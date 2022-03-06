af697acb2fb9442c36e27161bf30cffb6298498b
Tulio Rangel <tjrn20@hotmail.com>
2022-03-05 19:04:27 -0500

    add validation with Joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
-
 import { environments } from './environments';
 import config from './config';
 
@@ -18,6 +19,11 @@ import config from './config';
       envFilePath: environments[process.env.NODE_ENV] || '.env',
       load: [config],
       isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.number().required(),
+        DATA_BASE_NAME: Joi.string().required(),
+        DATA_BASE_PORT: Joi.number().required(),
+      }),
     }),
   ],
   controllers: [AppController],
