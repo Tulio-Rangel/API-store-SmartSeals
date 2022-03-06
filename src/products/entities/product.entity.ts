8446fad6ac2bca53863b1a8757a24fcaded658df
Tulio Rangel <tjrn20@hotmail.com>
2022-03-06 14:11:53 -0500

    create product schema

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,8 +1,22 @@
-export class Product {
-  id: number;
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongodb';
+
+@Schema()
+export class Product extends Document {
+  @Prop()
   name: string;
+
+  @Prop()
   description: string;
+
+  @Prop({ type: Number })
   price: number;
+
+  @Prop({ type: Number })
   stock: number;
+
+  @Prop()
   image: string;
 }
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
