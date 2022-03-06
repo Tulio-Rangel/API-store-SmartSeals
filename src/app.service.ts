c38a4654b0b3f5724f1d6a2ed05e7c9373b2f8d0
Tulio Rangel <tjrn20@hotmail.com>
2022-03-05 20:56:27 -0500

    apply format

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,9 @@ import config from './config';
 
 @Injectable()
 export class AppService {
-  constructor(@Inject(config.KEY) private configService: ConfigType<typeof config>) {}
+  constructor(
+    @Inject(config.KEY) private configService: ConfigType<typeof config>,
+  ) {}
   getHello(): string {
     const apiKey = this.configService.apiKey;
     const dataBase = this.configService.database.name;
